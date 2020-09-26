80ee98b8735d9927297e5d210745ce0890f6cc4f
daneshwar <daneshsgobbani@gmail.com>
2020-09-26 14:42:29 +0530

    patch1

--- autosub/main.py
+++ autosub/main.py
@@ -9,7 +9,7 @@ import argparse
 import subprocess
 import numpy as np
 from tqdm import tqdm
-from deepspeech import Model, version 
+# from deepspeech import Model, version 
 from segmentAudio import silenceRemoval
 from audioProcessing import extract_audio, convert_samplerate
 from writeToFile import write_to_file
@@ -35,31 +35,9 @@ def sort_alphanumeric(data):
     return sorted(data, key = alphanum_key)
 
 
-def ds_process_audio(ds, audio_file, file_handle):  
-    """Run DeepSpeech inference on each audio file generated after silenceRemoval
-    and write to file pointed by file_handle
-
-    Args:
-        ds : DeepSpeech Model
-        audio_file : audio file
-        file_handle : SRT file handle
-    """
-    
-    global line_count
-    fin = wave.open(audio_file, 'rb')
-    fs_orig = fin.getframerate()
-    desired_sample_rate = ds.sampleRate()
-    
-    # Check if sampling rate is required rate (16000)
-    if fs_orig != desired_sample_rate:
-        print("Warning: original sample rate ({}) is different than {}hz. Resampling might produce erratic speech recognition".format(fs_orig, desired_sample_rate), file=sys.stderr)
-        audio = convert_samplerate(audio_file, desired_sample_rate)
-    else:
-        audio = np.frombuffer(fin.readframes(fin.getnframes()), np.int16)
-
-    fin.close()
-    
+def ds_process_audio( audio_file, file_handle):  
     # Perform inference on audio segment
+    global line_count
     try:
         r=sr.Recognizer()
         with sr.AudioFile(audio_file) as source:
@@ -86,28 +64,28 @@ def main():
     print("AutoSub v0.1\n")
         
     parser = argparse.ArgumentParser(description="AutoSub v0.1")
-    parser.add_argument('--model', required=True,
+    parser.add_argument('--model', required=False,
                         help='DeepSpeech model file')
     parser.add_argument('--scorer',
                         help='DeepSpeech scorer file')
-    parser.add_argument('--file', required=True,
+    parser.add_argument('--file', required=False,
                         help='Input video file')
     args = parser.parse_args()
     
-    ds_model = args.model
-    if not ds_model.endswith(".pbmm"):
-        print("Invalid model file. Exiting\n")
-        exit(1)
+    # ds_model = args.model
+    # if not ds_model.endswith(".pbmm"):
+    #     print("Invalid model file. Exiting\n")
+    #     pass
     
-    # Load DeepSpeech model 
-    ds = Model(ds_model)
+    # # Load DeepSpeech model 
+    # ds = Model(ds_model)
             
-    if args.scorer:
-        ds_scorer = args.scorer
-        if not ds_scorer.endswith(".scorer"):
-            print("Invalid scorer file. Running inference using only model file\n")
-        else:
-            ds.enableExternalScorer(ds_scorer)
+    # if args.scorer:
+    #     ds_scorer = args.scorer
+    #     if not ds_scorer.endswith(".scorer"):
+    #         print("Invalid scorer file. Running inference using only model file\n")
+    #     else:
+    #         ds.enableExternalScorer(ds_scorer)
     
     input_file = args.file
     print("\nInput file:", input_file)
@@ -135,7 +113,7 @@ def main():
         
         # Dont run inference on the original audio file
         if audio_segment_path.split("/")[-1] != audio_file_name.split("/")[-1]:
-            ds_process_audio(ds, audio_segment_path, file_handle)
+            ds_process_audio(audio_segment_path, file_handle)
             
     print("\nSRT file saved to", srt_file_name)
     file_handle.close()
